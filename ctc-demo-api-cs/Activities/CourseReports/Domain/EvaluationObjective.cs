f3875a1a062140eb039e2c9e35f840e26506f8a7
Chris Boyd <christopher.boyd@withyouwithme.com>
2023-01-03 17:21:16 -0800

    docs: add default PO
    JSON file that can be loaded via Compass

--- ctc-demo-api-cs/Activities/CourseReports/Domain/EvaluationObjective.cs
+++ ctc-demo-api-cs/Activities/CourseReports/Domain/EvaluationObjective.cs
@@ -4,7 +4,7 @@ namespace WYWM.CTC.API.Activities.CourseReports.Domain;
 
 public class EvaluationObjective
 {
-    public BsonObjectId Id { get; set; } = null!;
     public string Name { get; set; } = null!;
     public bool Passed { get; set; }
+    public string? Comments { get; set; }
 }
